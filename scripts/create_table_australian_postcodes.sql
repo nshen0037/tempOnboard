6739252685a01ff7be5d525b8256b2cc6d274759
d-meadway <dmea0002@student.monash.edu>
2025-03-13 19:09:22 +1100

    Delete scripts/create_table_australian_postcodes.sql

--- scripts/create_table_australian_postcodes.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `recommendations` (
-  `id` int NOT NULL,
-  `uv` text,
-  `skin_type` text,
-  `rec_sunscreen` text,
-  `rec_clothing` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
